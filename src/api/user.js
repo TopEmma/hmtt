aeea656032dd2141b691c635a6c22956aceae925
TopEmma <1581917161@qq.com>
2022-06-26 18:04:24 +0800

    完成登录功能

---
+++ src/api/user.js
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+/*
+*获取短信验证码
+*@param {number} mobile
+*returns promise
+ *
+*/
+// export const getSmsCode = (mobile) => {
+//   return request({
+//     rul: `/sms/codes/${mobile}`
+//   })
+// }
+
+export const getSmsCode = (mobile) => {
+  return request({
+    url: `/sms/codes/${mobile}`
+  })
+}
+/*
+*登录
+*@param {mobile,code} param0
+*@return promise
+*/
+export const login = ({ mobile, code }) => {
+  return request({
+    method: 'POST',
+    url: '/authorizations',
+    data: {
+      mobile,
+      code
+    }
+  })
+}
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,20 +1,149 @@
 <template>
-  <div>登录页</div>
+  <div>
+    <van-nav-bar title="登录">
+      <!-- <van-icon name="cross" slot="left" /> -->
+      <!-- 以下是标准写法 -->
+      <!-- v-slot:left和#left效果一样 -->
+      <template #left>
+        <van-icon name="cross" />
+      </template>
+    </van-nav-bar>
+    <van-form @submit="onSubmit" ref="form">
+      <van-field
+        v-model.trim="mobile"
+        name="mobile"
+        label="手机号"
+        placeholder="请输入手机号"
+        :rules="[
+          { required: true, message: '请输入手机号' },
+          { pattern: /^(?:(?:\+|00)86)?1\d{10}$/, message: '手机号格式不正确' },
+        ]"
+      >
+        <i class="toutiao toutiao-shouji" slot="label"></i>
+      </van-field>
+      <van-field
+        v-model.trim="code"
+        type="text"
+        name="code"
+        label="验证码"
+        placeholder="请输入验证码"
+        :rules="[
+          { required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '验证码长度必须为6位' },
+        ]"
+      >
+        <i class="toutiao toutiao-yanzhengma" slot="label"></i>
+        <template #button>
+          <!-- 倒计时 -->
+          <van-count-down
+            v-if="isCountDownShow"
+            :time="time"
+            format="ss s"
+            @finish="isCountDownShow = false"
+          />
+          <van-button
+            @click="onSendSms"
+            v-else
+            size="small"
+            class="yzm"
+            native-type="button"
+            >发送验证码</van-button
+          >
+        </template>
+      </van-field>
+      <div style="margin: 16px">
+        <van-button round block native-type="submit" class="login-btn"
+          >提交</van-button
+        >
+      </div>
+    </van-form>
+  </div>
 </template>
 
 <script>
+import { getSmsCode, login } from '@/api/user'
 export default {
   created () { },
   data () {
-    return {}
+    return {
+      mobile: '13911111111', // 手机号
+      code: '246810', // 短信验证码
+      time: 5 * 1000,
+      isCountDownShow: false
+    }
   },
-  methods: {},
+  methods: {
+    async onSubmit (values) {
+      try {
+        const res = await login(values)
+        console.log(res)
+        // res.data.data
+        // token有效期 2小时，两种ilu：让用户重新登录 refresh_token，拿一个新的token
+        this.$store.commit('setUser', res.data.data)
+      } catch (err) {
+        console.log(err)
+      }
+      // console.log(123)
+      // console.log('submit', values)
+    },
+    async onSendSms () {
+      try {
+        await this.$refs.form.validate('mobile')
+        console.log('校验通过')
+        this.isCountDownShow = true // 显示倒计时
+        try {
+          await getSmsCode(this.mobile)
+          this.$toast.success('发送成功')
+        } catch (err) {
+          console.log(err)
+          this.$toast.fail('发送失败，请重试')
+        }
+      } catch (err) {
+        console.log(err)
+        console.log('校验失败')
+        // 轻提示
+        this.$toast.fail('手机号格式不正确')
+      }
+    }
+  },
   computed: {},
   watch: {},
   filters: {},
   components: {}
+
 }
 </script>
 
 <style scoped lang='less'>
+.toutiao {
+  font-size: 37px;
+}
+.yzm {
+  width: 152px;
+  height: 46px;
+  // line-height: 46px;
+  background-color: #ededed;
+  border-radius: 23px;
+  font-size: 22px;
+  color: #666;
+  .van-button__text {
+    zoom: 0.95;
+  }
+}
+.login-btn {
+  width: 694px;
+  height: 88px;
+  background-color: #6db4fb;
+  border-radius: 10px;
+  color: #fff;
+  font-size: 30px;
+}
+.van-count-down {
+  position: fixed;
+  right: 18px;
+}
+.yzm {
+  position: fixed;
+  right: 10px;
+}
 </style>
